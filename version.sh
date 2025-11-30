63f309ab55440f5dd870d595e46be2b5b62d89e6
iluha168 <illya.chernyshov888@gmail.com>
2025-12-01 01:16:29 +0700

    fix(version script): do not affect `dev` branch from `main-cjs`

--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-git checkout main
+git checkout main-cjs
 git merge --no-ff dev
 
 if ! [ -z $(git status --untracked-files=no --porcelain) ]; then
@@ -15,10 +15,3 @@ sed -E "s/(\"version\": \")[^\"]+(\")/\\1${NEW_VERSION:1}\\2/" -i deno.json
 git add -A
 git commit -m "${NEW_VERSION:1}"
 git tag "$NEW_VERSION" HEAD -m "$NEW_VERSION"
-
-if ! [ -z $(git status --untracked-files=no --porcelain) ]; then
-    echo "Git status is not clean" > /dev/stderr
-    exit 1
-fi
-git checkout dev
-git merge --no-ff main
